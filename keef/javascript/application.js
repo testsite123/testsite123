eb3b175345559ae6e173e9324b88e81cf20a4706
bohdan <zlatan93@mail.ua>
2016-10-20 20:49:35 +0000

    Initial commit

--- keef/javascript/application.js
+++ keef/javascript/application.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
 		$(".lang-select").toggle()
 	});
 	// end header langmenu
+	
+	// vertical-menu
+	$(".lang-vertical").click(function(){
+		$(".vertical-select").toggle()
+	});
+	// end vertical-menu
 
 	// portfolio height
 	$(".see-all-show").click(function(){
@@ -26,6 +32,14 @@ $(document).ready(function(){
 	// end portfolio height
 
 	// bxslider main page
-	$('.bxslider').bxSlider();
+	$('.bxslider').bxSlider({
+	});
 	// end bxslider main page
+
+	// adding class .filter-active to .filter-block
+	$(".filter-block").click(function(){
+		$(this).addClass("filter-active")
+	});
+	// end adding class .filter-active to .filter-block
+
 });

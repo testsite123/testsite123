e988e745b11d9f7a77e5ec44ce0ae6b2e1a84101
bohdan <zlatan93@mail.ua>
2016-10-20 09:16:50 +0000

    initial commit

--- keef/javascript/application.js
+++ keef/javascript/application.js
@@ -1,7 +1,31 @@
-$(".lang-menu").click(function(){
-	$(".lang-select").toggle();
-});
+$(document).ready(function(){
+	// header langmenu
+	$(".lang-menu").click(function(){
+		$(".lang-select").toggle()
+	});
+	// end header langmenu
 
-$(".lang-vertical").click(function(){
-	$(".vertical-select").toggle();
+	// portfolio height
+	$(".see-all-show").click(function(){
+		$(".see-all-visibility").attr({
+			style: "display:none"
+		});
+		$(".portfolio").attr({
+			style: "height:100%; overflow:visible"
+		});
+	});
+
+	$(".up-btn-show").click(function(){
+		$(".portfolio").attr({
+			style: "height:582px; overflow:hidden"
+		});
+		$(".see-all-visibility").attr({
+			style: "display:block"
+		});
+	});
+	// end portfolio height
+
+	// bxslider main page
+	$('.bxslider').bxSlider();
+	// end bxslider main page
 });

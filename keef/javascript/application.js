bbe990d0c641239d460bf0b89e95fbb49b90741b
bohdan <zlatan93@mail.ua>
2016-10-25 09:35:54 +0000

    Initial commit

--- keef/javascript/application.js
+++ keef/javascript/application.js
@@ -1,41 +1,38 @@
 $(document).ready(function(){
 	// header langmenu
 	$(".lang-menu").click(function(){
-		$(".lang-select").toggle()
+		$(".lang-select").toggle(400)
 	});
 	// end header langmenu
 	
 	// vertical-menu
 	$(".lang-vertical").click(function(){
-		$(".vertical-select").toggle()
+		$(".vertical-select").toggle(400)
 	});
 	// end vertical-menu
 
 	// portfolio height
+	var portsecHeight = $(".portfolio-section").height();
+	var doubleHeight = portsecHeight*2;
+
 	$(".see-all-show").click(function(){
-		$(".see-all-visibility").attr({
-			style: "display:none"
-		});
-		$(".portfolio").attr({
-			style: "height:100%; overflow:visible"
-		});
+		$(".see-all-visibility").css("display", "none");
+		$(".portfolio").css("height", "100%").css("overflow", "visible");
+		$(".paralax").css("margin-top", "0");
 	});
 
 	$(".up-btn-show").click(function(){
-		$(".portfolio").attr({
-			style: "height:582px; overflow:hidden"
-		});
-		$(".see-all-visibility").attr({
-			style: "display:block"
-		});
+		$(".portfolio").css("height", doubleHeight).css("overflow", "hidden");
+		$(".see-all-visibility").css("display", "block");
+		// $(".paralax").css("margin-top", "-200px");
 	});
 	// end portfolio height
 
-	// bxslider main page
-	$('.bxslider').bxSlider({
-	});
-	// end bxslider main page
 
+
+	// bxslider main page
+	$(".header-slider").bxSlider();
+	// end bxslider main page
 	// adding class .filter-active to .filter-block
 	$(".filter-block").click(function(){
 		$(this).addClass("filter-active")

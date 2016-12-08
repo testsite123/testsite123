54e391f7ff7840b6ac95097f486e9465add9feb0
bohdan <zlatan93@mail.ua>
2016-12-08 10:47:02 +0000

    Initial commit

---
+++ fiw_mobile/javascript/mobile.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	// header-menu
+	$(".menu-open").click(function(){
+		$(".m-menu").addClass("m-menu-active");
+		$(".menu-open").addClass("m-hidden");
+		$(".menu-close").removeClass("m-hidden");
+	});
+
+	$(".menu-close, a").click(function(){
+		$(".m-menu").removeClass("m-menu-active");
+		$(".menu-open").removeClass("m-hidden");
+		$(".menu-close").addClass("m-hidden");
+	});
+	// end header-menu
+
+	// portfolio resizer
+	$(".m-resizer").click(function(){
+		$(this).addClass("m-hidden");
+		$(".m-portfolio-wrapper").removeClass("m-hidden");
+		$(".m-portfolio-wrapper").css("margin-top", "2px");
+	});
+	// end portfolio resizer
+
+});
